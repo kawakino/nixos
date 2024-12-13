396eab4fd763436120e877333bde13e3b2029638
Your Name <you@example.com>
2024-12-13 18:58:48 +0100

    feat: add backup configuration for home-manager

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";  # добавляем это
             home-manager.users.cizen = import ./home/home.nix;
           }
         ];
