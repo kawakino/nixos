df579539b7aebf1e458d10fed6aa096d21f9bed2
Your Name <you@example.com>
2024-12-14 14:56:41 +0100

    fix: update nerd-fonts syntax

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: {
   fonts.packages = with pkgs; [
     jetbrains-mono
-    nerd-fonts.jetbrainsmono
+    nerd-fonts.jetbrains-mono
     noto-fonts
     noto-fonts-emoji
   ];
